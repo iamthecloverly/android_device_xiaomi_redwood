65153dc60d56ae87ebc5600b44f3300a9a24cc28
TheStrechh <TheStrechh@gmail.com>
2023-06-30 20:01:05 +0530

    redwood: vibrator: rebrand to redwood
    * Drop unused qti vibrator effect
    * Add awinic_haptic support

--- common.mk
+++ common.mk
@@ -483,7 +483,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_sm8350
+    vendor.qti.hardware.vibrator.service.redwood
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
