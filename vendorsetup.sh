341a0402de1b4ef66d5cab18422fb8c63cc9802f
Sribalaji <iam.thecloverly@pm.me>
2023-07-13 18:32:37 +0000

    redwood: add vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/iamthecloverly/android_device_xiaomi_redwood device/xiaomi/redwood
+
+git clone https://github.com/xiaomi-redwood-devs/android_kernel_xiaomi_redwood --depth=1 kernel/xiaomi/redwood
+
+git clone https://github.com/xiaomi-redwood-devs/proprietary_vendor_xiaomi_redwood --depth=1 vendor/xiaomi/redwood
+
+git clone https://github.com/xiaomi-redwood-devs/redwood_firmware vendor/xiaomi/redwood-firmware
